883b2412a57813ad4a8c1a0f3a31ffc740a12cdb
Èric Bitriá Ribes <eric.bitria@gmail.com>
2026-05-02 23:14:53 +0200

    UI: Center login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -48,8 +48,8 @@ export default function LoginPage() {
 
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <div className="flex flex-col items-center w-full gap-6 text-center sm:items-start sm:text-left">
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
+                <div className="flex flex-col items-center w-full gap-6 text-center">
                     <Card className="w-full max-w-md">
                         <CardHeader>
                             <CardTitle>login</CardTitle>
